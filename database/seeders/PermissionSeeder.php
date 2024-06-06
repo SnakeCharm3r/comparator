bb7aab6085ee44d89b1b5bef94cadf06965cc404
sheby460 <59543240+sheby460@users.noreply.github.com>
2024-06-06 10:59:03 +0300

    roles and permission seeder and assignments

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Permission;
 class PermissionSeeder extends Seeder
 {
     /**
@@ -12,6 +12,25 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       // Reset cached roles and permissions
+       app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+       // List of permissions to create
+       $permissions = [
+           'submit form',
+           'view forms',
+           'user dashboard',
+           'view own forms',
+           'check form status',
+           'approve hr',
+           'approve it',
+           'approve hod',
+           'approve acting hod',
+       ];
+
+       // Create permissions if they do not already exist
+       foreach ($permissions as $permission) {
+           Permission::firstOrCreate(['name' => $permission]);
+       }
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-// use Spatie\Permission\Contracts\Role;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 
@@ -15,12 +15,22 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create(['name' => 'employee']);
-        Role::create(['name' => 'level1_approver']);
-        Role::create(['name' => 'level2_approver']);
-        Role::create(['name' => 'administrator']);
+    // Reset cached roles and permissions
+    app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-      
+    // List of roles and their permissions
+    $roles = [
+        'requester' => ['submit form', 'user dashboard', 'view forms', 'view own forms', 'check form status'],
+        'head of hr' => ['approve hr','submit form', 'user dashboard', 'view forms', 'view own forms', 'check form status'],
+        'head of it' => ['approve it','approve hr','submit form', 'user dashboard', 'view forms', 'view own forms', 'check form status'],
+        'head of department' => ['approve hod','submit form', 'user dashboard', 'view forms', 'view own forms', 'check form status'],
+        'acting hod' => ['approve hr', 'approve it', 'approve hod'],
+    ];
 
+    // Create roles and assign permissions if they do not already exist
+    foreach ($roles as $roleName => $permissions) {
+        $role = Role::firstOrCreate(['name' => $roleName]);
+        $role->syncPermissions($permissions);
+      }
     }
 }
