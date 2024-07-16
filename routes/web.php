b7a8f90e7effa0625e44f3fdc56408416762b32e
sheby460 <59543240+sheby460@users.noreply.github.com>
2024-07-16 11:59:26 +0300

    chuma

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::get('/departments', [DepartmentController::class, 'index']);
 
 
 
- Route::get('/family-details', [UserFamilyDetailsController::class, 'index'])->name('index');
+ Route::get('/family-details', [UserFamilyDetailsController::class, 'index'])->name('family-details.index');
 Route::post('/familyData', [UserFamilyDetailsController::class, 'addFamilyData'])->name('family-details.addFamilyData');
 Route::get('/health-details', [HealthDetailsController::class, 'index'])->name('health-details.index');
 Route::post('/health', [HealthDetailsController::class, 'addHealthData'])->name('health-details.addHealthData');
