f3fc09a283477243d64d7e45a261009ea868ea01
dinocleo <104690785+dinocleo@users.noreply.github.com>
2024-07-17 15:14:20 +0300

    Revert "Merge pull request #87 from sheby460/chumaBranch"
    This reverts commit 6ecf0c1e032b3fc08ecdfdb694796a44ff1815cc, reversing
    changes made to 1e42e07bc1d00c00799513ab1d766180d5682011.

--- routes/web.php
+++ routes/web.php
@@ -70,22 +70,14 @@ Route::get('/departments', [DepartmentController::class, 'index']);
 
 
 
- Route::get('/family-details', [UserFamilyDetailsController::class, 'index'])->name('family-details.index');
+Route::get('/family-details', [UserFamilyDetailsController::class, 'index'])->name('family-details.index');
 Route::post('/familyData', [UserFamilyDetailsController::class, 'addFamilyData'])->name('family-details.addFamilyData');
-
-
 Route::get('/health-details', [HealthDetailsController::class, 'index'])->name('health-details.index');
 Route::post('/health', [HealthDetailsController::class, 'addHealthData'])->name('health-details.addHealthData');
-Route::get('/health-details/{id}/edit', 'HealthDetailsController@edit')->name('health-details.edit');
-Route::delete('/health-details/{id}', 'HealthDetailsController@delete')->name('health-details.delete');
-
-
-
 Route::get('/ccbrt_relation', [CcbrtRelationController::class, 'index'])->name('ccbrt_relation.index');
 Route::post('/relation', [CcbrtRelationController::class, 'addRelationData'])->name('ccbrt_relation.addRelationData');
 Route::get('/language-knowledge', [LanguageKnowledgeController::class, 'index'])->name('language_knowledge.index');
 Route::post('/language-knowledge', [LanguageKnowledgeController::class, 'addLanguageKnowledge'])->name('language_knowledge.add');
-
 
 // Route::post('family-details', UserFamilyDetailsController::class);
 // Route::post('/healthDetails', [UserFamilyDetailsController::class, 'addHealthData'])->name('healthDetails.addHealthData');
