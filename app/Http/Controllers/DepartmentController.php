31743f2aea1eb1fbc1afc9bcdff636e0521b5ef6
sheby460 <59543240+sheby460@users.noreply.github.com>
2024-08-06 15:08:15 +0300

    hod commit

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Departments;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use RealRashid\SweetAlert\Facades\Alert;
 
@@ -12,12 +13,41 @@ class DepartmentController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index1()
     {
         $departments = Departments::where('delete_status', 0)->get();
         return view('department.index', compact('departments'));
 
     }
+
+    public function index()
+    {
+        $departments = DB::table('departments')
+            ->leftJoin('users', 'departments.id', '=', 'users.deptId')
+            ->leftJoin('model_has_roles', 'users.id', '=', 'model_has_roles.model_id')
+            ->leftJoin('roles', 'model_has_roles.role_id', '=', 'roles.id')
+            ->select(
+                'departments.id as dept_id',
+                'departments.dept_name',
+                'departments.description',
+                'departments.delete_status',
+                DB::raw('CONCAT(users.fname, " ", users.lname) as head_of_department')
+            )
+            ->where('departments.delete_status', 0)
+            ->where('roles.name', 'line-manager')
+            ->get();
+
+        return view('department.index', compact('departments'));
+    }
+
+    // public function index1(){
+    //     $sops = DB::table('sops')->join('departments',
+    //     'sops.deptId', '=', 'departments.id')
+    //     ->select(
+    //         'sops.*', 'departments.dept_name'
+    //     )->get();
+    //     return view('sops.index', compact('sops'));
+    // }
 
     /**
      * Show the form for creating a new resource.
@@ -55,7 +85,7 @@ class DepartmentController extends Controller
         $dept = Departments::create([
             'dept_name' => $request->input('dept_name'),
             'description' => $request->input('description'),
-            'delete_status' => 0, 
+            'delete_status' => 0,
         ]);
         Alert::success('Department added successful','Department Added');
         return redirect()->route('department.index')->with('success', 'Department added successfully.');
@@ -95,25 +125,25 @@ class DepartmentController extends Controller
             'dept_name' => 'required',
             'description' => 'required',
         ]);
-    
+
         $validator = Validator::make($request->all(), [
             'dept_name' => 'required',
             'description' => 'required',
         ]);
-    
+
         if ($validator->fails()) {
             return response()->json([
                 'status' => 400,
                 'errors' => $validator->errors(),
             ]);
         }
-    
+
         $department = Departments::findOrFail($id);
         $department->update([
             'dept_name' => $request->input('dept_name'),
             'description' => $request->input('description'),
         ]);
-    
+
         Alert::success('Department Updated successful','Department updated');
         return redirect()->route('department.index')->with('success', 'Department updated successfully.');
     }
@@ -143,19 +173,19 @@ public function destroy(string $id)
     // public function softDelete(Request $request)
     // {
     //     $dept = Departments::find($request->id);
-    
+
     //     if (!$dept) {
     //         return response()->json([
     //             'status' => 404,
     //             'message' => 'Department not found',
     //         ]);
     //     }
-    
+
     //     $dept->update([
     //         'delete_status' => 1
     //     ]);
-    
+
     //     return redirect()->route('department.index')->with('success', 'Department deleted successfully.');
-    // }    
-    
+    // }
+
 }
