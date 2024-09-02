49671044be03aeb579b41ee90865f6c3af265036
sheby460 <59543240+sheby460@users.noreply.github.com>
2024-09-02 14:37:47 +0300

    mail message commit

--- resources/views/emails/approval_request_notification.blade.php
+++ resources/views/emails/approval_request_notification.blade.php
@@ -1,9 +1,12 @@
-<p>Hello, {{ $approver->fname }}</p>
-<p>You have a new request to approve.</p>
+<p>Dear, {{ $approver->fname }}</p>
+<p>You have a new request awaiting your approval.</p>
 <p>Request Details:</p>
 <ul>
-    <li>User : {{ $requestDetails['attended_by'] }}</li>
-    <li>Request : {{ $requestDetails['requestId'] }}</li>
-    <li>Request Date: {{ $requestDetails['requestDate'] }}</li>
+    <li>Requested By : {{ $requestDetails['attended_by'] }}</li>
+    <li>Requested Form : {{ $requestDetails['requestId'] }}</li>
+    <li>Date of Request: {{ $requestDetails['requestDate'] }}</li>
 </ul>
-<p>Please log in to the system to approve the request.</p>
+<p>Please log in to e-Docs system to approve the request .</p>
+<p>Using the below link <a href="http://127.0.0.1:8000/"> e-Docs System</a></p>
+<p>Regards</p>
+<p>CCBRT IT Support Desk</p>
