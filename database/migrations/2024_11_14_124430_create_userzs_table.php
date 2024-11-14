ed914b10761fb0d826ee751c0451662f394633b6
Simon Kaswija <143195354+SnakeCharm3r@users.noreply.github.com>
2024-11-14 17:48:38 +0300

    updated userz table

--- database/migrations/2024_11_14_124430_create_userzs_table.php
+++ database/migrations/2024_11_14_124430_create_userzs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use FontLib\Table\Type\name;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,6 +15,10 @@ return new class extends Migration
         Schema::create('userzs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string(column: 'name');
+            $table->string(column: 'email');
+            $table->string('password');
+
         });
     }
 
